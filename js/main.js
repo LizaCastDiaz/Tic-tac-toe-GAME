d6a7fbe50d16fefa8fed482950d7cb9a60685915
Liza <Lizacastdiaz@gmail.com>
2022-10-12 20:11:19 +0200

    He agregado funcionalidad a las opciones del jugador. Lo he agregado al localstorage. Tambien he terminado la parte de las condiciones para ganar en la mesa de juego

--- js/main.js
+++ js/main.js
@@ -1,22 +1,15 @@
-let tablero = [];
+let tablero = ["","","","","","","","",""];
 let jugadorActivo = "X";
 let gameActive = true;
+
 const celdas = document.querySelectorAll(".celda");
 const boardDisplay = document.querySelector(".game--status");
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+
 
 function gestionClick(event){
     const celdaActiva = event.target;
     const celdaNumero = parseInt(celdaActiva.getAttribute('data-celda')); 
+
 
     // REVISAR SI LA CELDA ESTÁ VACÍA
     // SI ESTÁ VACÍA, CONTINUAR MARCANDO LA POSICIÓN
@@ -27,8 +20,16 @@ function gestionClick(event){
         return;
     }
 
+    console.log(jugadorActivo);  
+
     // MARCAR CELDA CON SÍMBOLO DEL JUGADOR EN EL ARRAY
     tablero[celdaNumero] = jugadorActivo;
+
+    
+    resultValidation();
+
+    
+    console.log(tablero);
 
     // MARCAR CELDA CON SÍMBOLO DEL JUGADOR ACTIVO
     celdaActiva.innerHTML = jugadorActivo;
@@ -40,32 +41,49 @@ function gestionClick(event){
         jugadorActivo = "X";
     }
 
-
-    console.log(tablero);
 }
 
 
 celdas.forEach(celda => celda.addEventListener("click", gestionClick));
 
 
-function ResultValidation(){
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function resultValidation(){
+
     let roundWon = false;
     for (let i = 0; i <= 7; i++){
-        const winningConditions = winningConditions[i];
-        let a = celdas[winCondition[0]];
-        let b = celdas[winCondition[1]];
-        let c = celdas[winCondition[2]];
+        const winCondition = winningConditions[i];
+
+        console.log(winningConditions[i]);
+
+        let a = tablero[winCondition[0]];
+        let b = tablero[winCondition[1]];
+        let c = tablero[winCondition[2]];
+
+      
 
         if (a === '' || b === '' || c === '') {
             continue;
         }
+
         if (a === b && b === c){
             roundWon = true;
             break
         }
     }
+
     if (roundWon) {
-        boardDisplay.innerHTML = victoryMessage();
+        boardDisplay.innerHTML = "Ganó";
         gameActive = false;
         return;
     }
