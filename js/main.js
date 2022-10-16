4db4477953e2100f3f9242f5e70be742f8cce858
Liza <Lizacastdiaz@gmail.com>
2022-10-16 15:16:42 +0200

    he hecho una funcion para el cpu. La idea es q se marque sola en un espacio libre. Aun no he podido con ello, ps no funciona

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 //VARIABLES
 let oContador = 0;
 let xContador = 0;
-let tablero = ["","","","","","","","",""];
+let tablero = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+];
 let jugadorActivo = "X";
-let random;
-let empyCells = tablero;
 let gameActive = true;
 let playerOneName = localStorage.getItem("firstPlayer");
 let playerSecondName = localStorage.getItem("secondPlayer");
@@ -12,123 +20,113 @@ let playerSecondName = localStorage.getItem("secondPlayer");
 const celdas = document.querySelectorAll(".celda");
 const boardDisplay = document.querySelector(".game--status");
 
+///CPU
+function computerMove() {
+    let emptyCellsIndex = [];
+    for (let i = 0; i < tablero.length; i++) {
+        if (tablero[i] === "") {
+            emptyCellsIndex.push(i);
+    
+        }
+    const random = Math.ceil(Math.random() * emptyCellsIndex.length) - 1;
+    emptyCellsIndex[random].oContainer = jugadorActivo;
+    resultValidation();
+    jugadorActivo();
+  }
+    }
+    console.log(computerMove);
+
+    
 
 
-
-
-function gestionClick(event){
+function gestionClick(event) {
     const celdaActiva = event.target;
-    const celdaNumero = parseInt(celdaActiva.getAttribute('data-celda')); 
+    const celdaNumero = parseInt(celdaActiva.getAttribute('data-celda'));
     const xContainer = document.querySelector('#xContainer');
     const oContainer = document.querySelector('#oContainer');
 
-    // if (jugadorActivo >= 0){
-    //     xContador +
-        
-    // } else{
-    //     oContador
-    // }
-
- //si el jugador activo llega hasta la tercera X entoces no puedo seguir jugando y debe de quitar 1.
-
-
-
-    // REVISAR SI LA CELDA ESTÁ VACÍA
-    // SI ESTÁ VACÍA, CONTINUAR MARCANDO LA POSICIÓN
+    // REVISAR SI LA CELDA ESTÁ VACÍA SI ESTÁ VACÍA, CONTINUAR MARCANDO LA POSICIÓN
     // EN EL ARRAY Y EN EL TABLERO VISIBLE
     if (celdaActiva.innerHTML !== "") {
-        console.log('celda ya está ocupada');
+
         return;
     }
-
-    console.log(jugadorActivo);  
 
     // MARCAR CELDA CON SÍMBOLO DEL JUGADOR EN EL ARRAY
     tablero[celdaNumero] = jugadorActivo;
 
-    
     resultValidation();
-
-    
-    console.log(tablero);
 
     // MARCAR CELDA CON SÍMBOLO DEL JUGADOR ACTIVO
     celdaActiva.innerHTML = jugadorActivo;
 
     // CAMBIAR JUGADOR EN TURNO
-    if(jugadorActivo === "X"){
+    if (jugadorActivo === "X") {
         jugadorActivo = "O";
     } else {
         jugadorActivo = "X";
     }
 
+    // OCULTAR SÍMBOLO DEL JUGADOR ACTIVO AL CAMBIAR DE TURNO SI LA VARIABLE
+    // jugadorActivo ES IGUAL A "X", ENTONCES OCULTAR EL CONTENEDOR DEL SÍMBOLO "O"
+    // Y VICEVERSA USAR classList POR EJEMPLO:
+    xContainer
+        .classList
+        .toggle('visually-hidden');
+    oContainer
+        .classList
+        .toggle('visually-hidden');
 
-    // OCULTAR SÍMBOLO DEL JUGADOR ACTIVO AL CAMBIAR DE TURNO
-
-
-    // SI LA VARIABLE jugadorActivo ES IGUAL A "X", ENTONCES
-    // OCULTAR EL CONTENEDOR DEL SÍMBOLO "O" Y VICEVERSA
-    // USAR classList POR EJEMPLO:
-    xContainer.classList.toggle('visually-hidden');
-    oContainer.classList.toggle('visually-hidden');
-
-   
-
-
-
-
-
+    computerMove();
 
 }
-
- // COMPUTER GAME
-function computerMove(){
-    tablero.forEach(function(tablero){
-        if (tablero.random == ''){
-            empyCells.push(tablero);
-        }
-    })
-}
-
-random = Math.ceil(Math.random() * empyCells.length) - 1;
-empyCells[random].textContent = O;
-
-
 
 //WINNING CONDITIONS
 
 celdas.forEach(celda => celda.addEventListener("click", gestionClick));
 
 const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [
+        0, 1, 2
+    ],
+    [
+        3, 4, 5
+    ],
+    [
+        6, 7, 8
+    ],
+    [
+        0, 3, 6
+    ],
+    [
+        1, 4, 7
+    ],
+    [
+        2, 5, 8
+    ],
+    [
+        0, 4, 8
+    ],
+    [
+        2, 4, 6
+    ]
 ];
 
-function resultValidation(){
+function resultValidation() {
 
     let roundWon = false;
-    for (let i = 0; i <= 7; i++){
+    for (let i = 0; i <= 7; i++) {
         const winCondition = winningConditions[i];
-
-        console.log(winningConditions[i]);
 
         let a = tablero[winCondition[0]];
         let b = tablero[winCondition[1]];
         let c = tablero[winCondition[2]];
 
-      
-
         if (a === '' || b === '' || c === '') {
             continue;
         }
 
-        if (a === b && b === c){
+        if (a === b && b === c) {
             roundWon = true;
             break
         }
@@ -137,18 +135,19 @@ function resultValidation(){
     if (roundWon) {
         let winnerName;
 
-        if(jugadorActivo === 'X') {
+        if (jugadorActivo === 'X') {
             winnerName = playerOneName;
         } else {
             winnerName = playerSecondName;
         }
 
-        boardDisplay.innerHTML = "Ganó " + winnerName; // Quien gano?
+        boardDisplay.innerHTML = "Winner " + winnerName; // Quien gano?
         gameActive = false;
         return;
     }
 }
 
+//Nombre de los jugadores
 function pageLoad() {
     let playerOneContainer = document.querySelector("#playerOneName");
     let playerSecondContainer = document.querySelector("#playerSecondName");
@@ -157,12 +156,16 @@ function pageLoad() {
     playerSecondContainer.innerHTML = playerSecondName;
 }
 
-window.onload = pageLoad();
+
+
+
 
 
 
 /////RESET BUTTON/////
-
-function refreshPage(){
-    window.location.reload();
+window.onload = pageLoad();
+function refreshPage() {
+    window
+        .location
+        .reload();
 }
